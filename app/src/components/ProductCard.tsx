f4967df6eb89fe7732784e2b4b45fdb8e1a8760c
David <daviolivero2103@gmail.com>
2026-07-29 14:39:40 -0400

    feat: add settings to display images

--- app/src/components/ProductCard.tsx
+++ app/src/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Product } from "../schemas/schemas";
 import { formatCurrency } from "../utils/currency";
 
@@ -7,7 +8,12 @@ export default function ProductCard({product}: {product: Product}) {
             className='rounded bg-white shadow relative p-5'
         >
             <div>
-
+                <Image
+                    src={`${process.env.API_URL}/img/${product.image}`}
+                    alt={`Imagen del producto ${product.name}`}
+                    width={400}
+                    height={600}
+                />
                 <div className="p-3 space-y-2">
                     <h3 className="text-xl font-bold text-gray-600">{product.name}</h3>
                     <p className="text-gray-500">Disponibles: {product.inventory}</p>
