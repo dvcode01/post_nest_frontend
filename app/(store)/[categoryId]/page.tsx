0599cf54e4efeb1f13dad9629ee7bd4b5cd0789a
David <daviolivero2103@gmail.com>
2026-07-28 21:18:01 -0400

    feat(store): get products from the api

---
+++ app/(store)/[categoryId]/page.tsx
@@ -0,0 +1,19 @@
+
+type params = Promise<{categoryId: string}>;
+
+async function getProducts(categoryId: string){
+    const url = `${process.env.API_URL}/categories/${categoryId}?products=true`;
+    const req = await fetch(url);
+
+    const json = await req.json();
+    return json;
+}
+
+export default async function StorePage({params}: {params: params}) {
+    const { categoryId } = await params;
+    const products = await getProducts(categoryId);
+    
+    return (
+        <div>StorePage {categoryId}</div>
+    )
+}
