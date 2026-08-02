cba7a33bb3f8a2fa3091ff2ad5811c0a779019d3
David <daviolivero2103@gmail.com>
2026-08-01 20:06:54 -0400

    chore(stores): transferring products to the store

--- app/src/stores/store.ts
+++ app/src/stores/store.ts
@@ -1,9 +1,14 @@
 import { create } from "zustand";
+import { Product } from "../schemas/schemas";
 
 interface store {
-
+    total: number;
+    addToCart: (product: Product) => void;
 };
 
 export const useStore = create<store>(() => ({
-
+    total: 0,
+    addToCart(product) {
+        console.log(product);
+    },
 }));
