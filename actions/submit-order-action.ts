a1b91946eee5ad1fd7d0b56494b66cbf2492e6c4
David <daviolivero2103@gmail.com>
2026-08-06 21:26:58 -0400

    feat(actions): add server action to store the orders

---
+++ actions/submit-order-action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+
+export async function submitOrder(){
+    console.log('desde submit order')
+
+    return {
+        errors: [],
+        success: ''
+    }
+}
+
+
+
+
--- app/src/components/cart/SubmitOrderForm.tsx
+++ app/src/components/cart/SubmitOrderForm.tsx
@@ -1,7 +1,25 @@
+import { submitOrder } from "@/actions/submit-order-action"
+import { useActionState } from "react"
+import { useStore } from "../../stores/store";
 
 export default function SubmitOrderForm() {
+    const total = useStore(state => state.total);
+    const contents = useStore(state => state.contents);
+    const coupon = useStore(state => state.coupon.name);
+
+    const order = {
+        total,
+        contents, 
+        coupon
+    };
+
+    const [state, dispatch] = useActionState(submitOrder, {
+        errors: [],
+        success: ''
+    });
+
     return (
-        <form>
+        <form action={dispatch}>
             <input 
                 type="submit"
                 className="mt-5 w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase p-3" 
