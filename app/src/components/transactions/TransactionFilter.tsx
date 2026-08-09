5232f4355409044980f3d197be72a368dedfe466
David <daviolivero2103@gmail.com>
2026-08-09 19:38:39 -0400

    feat(components): add calendar to filter dates

--- app/src/components/transactions/TransactionFilter.tsx
+++ app/src/components/transactions/TransactionFilter.tsx
@@ -1,17 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function TransactionFilter() {
-  return (
-    <div>
-        <div className="">
-            <Calendar />
-        </div>
+    const [date, setDate] = useState<Value>(new Date());
 
-        <div className="">
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-8">
+            <div className="">
+                <Calendar onChange={setDate} value={date} />
+            </div>
 
+            <div className="">
+                3
+            </div>
         </div>
-    </div>
-  )
+    )
 }
