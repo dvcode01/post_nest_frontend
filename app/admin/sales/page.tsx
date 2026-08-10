986c31c1a7dec5ac3de8668a48d4958b93016047
David <daviolivero2103@gmail.com>
2026-08-09 20:34:27 -0400

    chore: configuring react query

--- app/admin/sales/page.tsx
+++ app/admin/sales/page.tsx
@@ -1,14 +1,20 @@
 import TransactionFilter from '@/app/src/components/transactions/TransactionFilter'
 import Heading from '@/app/src/components/ui/Heading'
+import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query'
 
-export default function SalesPage() {
+export default async function SalesPage() {
+    const queryClient = new QueryClient();
+
     return (
         <>
            <Heading>Ventas</Heading>
 
            <p className='text-lg'>En esta sección consulta todas las ventas realizadas, utiliza el calendario para filtrarlas</p>
 
-           <TransactionFilter />
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <TransactionFilter />
+
+            </HydrationBoundary>
         </>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 const outfit = Outfit({subsets: ["latin"]});
 
@@ -19,7 +20,9 @@ export default function RootLayout({
       lang="en"
       className={`${outfit.className} bg-gray-200`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
