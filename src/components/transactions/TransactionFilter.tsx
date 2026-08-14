75fe3bbca52231ffedf4ebf35a32162c86ef7399
David <daviolivero2103@gmail.com>
2026-08-13 22:48:59 -0400

    chore: add text when loading data

--- src/components/transactions/TransactionFilter.tsx
+++ src/components/transactions/TransactionFilter.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { getSalesbyDate } from "@/src/lib/api";
+import { useQuery } from "@tanstack/react-query";
 import { format } from "date-fns";
 import { useState } from "react";
 import Calendar from "react-calendar";
@@ -11,7 +13,11 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 export default function TransactionFilter() {
     const [date, setDate] = useState<Value>(new Date());
     const formattedDate = format(date?.toString()! || new Date(), 'yyyy-MM-dd');
-    console.log(formattedDate);
+    const { data, isLoading } = useQuery({
+        queryKey: ['sales', formattedDate],
+        queryFn: () => getSalesbyDate(formattedDate)
+    });
+    console.log(data)
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-8">
@@ -20,6 +26,7 @@ export default function TransactionFilter() {
             </div>
 
             <div className="">
+                {isLoading && <p>Cargando...</p>}
                 3
             </div>
         </div>
