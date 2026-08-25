f6f69da9dd92d79a82d403ab519e71198a56fac2
David <daviolivero2103@gmail.com>
2026-08-24 22:13:12 -0400

    feat(components): add upload file field in product form

--- src/components/products/ProductForm.tsx
+++ src/components/products/ProductForm.tsx
@@ -1,4 +1,5 @@
 import { CategoriesResponseApiSchema, Product } from "@/src/schemas/schemas";
+import UploadProductImage from "./UploadProductImage";
 
 async function getCategories(){
   const url = `${process.env.API_URL}/categories`;
@@ -82,6 +83,7 @@ export default async function ProductForm({product}: {product?: Product}) {
         </select>
       </div>
 
+      <UploadProductImage />
     </>
   )
 }
