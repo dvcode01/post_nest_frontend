91f192122305f5836c9ebaca78130e8f4aafcb4e
David <daviolivero2103@gmail.com>
2026-08-28 14:01:17 -0400

    chore: add imagePath in cart item

--- src/components/cart/ShoppingCartItem.tsx
+++ src/components/cart/ShoppingCartItem.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { CartItem } from "../../schemas/schemas";
 import { formatCurrency } from "../../utils/currency";
 import { useStore } from "../../stores/store";
+import { getImagePath } from "@/src/utils/image";
 
 export default function ShoppingCartItem({item}: {item: CartItem}) {
     const updateQuantity = useStore(state => state.updateQuantity);
@@ -11,7 +12,7 @@ export default function ShoppingCartItem({item}: {item: CartItem}) {
         <li className="flex items-center space-x-6 py-6 relative">
             <div className='h-24 w-24'>
                 <Image
-                    src={`${process.env.NEXT_PUBLIC_API_URL}/img/${item.image}`}
+                    src={getImagePath(item.image)}
                     alt={`Imagen del producto ${item.name}`}
                     width={100}
                     height={100}
