3030708c416407c54c90253835125eb615da8465
David <daviolivero2103@gmail.com>
2026-08-29 17:04:13 -0400

    chore: add new hostname in config

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: process.env.DOMAIN!, 
+        hostname: 'pos-nest-backend.onrender.com', 
       },
       {
         protocol: 'https',
