6075edd41f9382d2761f8b8e808247da5d2465df
David <daviolivero2103@gmail.com>
2026-09-05 21:05:34 -0400

    feat(admin): get all coupons

--- app/admin/coupons/page.tsx
+++ app/admin/coupons/page.tsx
@@ -1,6 +1,19 @@
 import Heading from "@/src/components/ui/Heading";
+import { CouponsResponseApiSchema } from "@/src/schemas/schemas";
+
+async function getCoupons(){
+  const url = `${process.env.API_URL}/coupons`;
+  const req = await fetch(url);
+
+  const json = await req.json();
+  const coupons = CouponsResponseApiSchema.parse(json);
+  return coupons;
+}
 
 export default async function CouponsPage() {
+  const coupons = await getCoupons();
+  console.log(coupons);
+
   return (
     <>
       <Heading>Administra tus Cupones</Heading>
