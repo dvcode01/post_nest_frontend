77c9d2f983890f52b965eb30abdc42d261c6188b
David <daviolivero2103@gmail.com>
2026-09-05 20:03:45 -0400

    feat(admin): add coupon page

---
+++ app/admin/coupons/page.tsx
@@ -0,0 +1,9 @@
+import Heading from "@/src/components/ui/Heading";
+
+export default async function CouponsPage() {
+  return (
+    <>
+      <Heading>Administra tus Cupones</Heading>
+    </>
+  )
+}
