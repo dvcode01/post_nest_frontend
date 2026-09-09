2279b4bbef00b1c18515bba7097dc86f81b863aa
David <daviolivero2103@gmail.com>
2026-09-09 14:03:52 -0400

    feat(admin): get coupon to edit

--- app/admin/coupons/[id]/edit/page.tsx
+++ app/admin/coupons/[id]/edit/page.tsx
@@ -1,7 +1,28 @@
 import Heading from "@/src/components/ui/Heading";
+import { CouponSchema } from "@/src/schemas/schemas";
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
-export default function EditCouponPage() {
+async function getCoupon(id: string) {
+    const url = `${process.env.API_URL}/coupons/${id}`;
+    const req = await fetch(url);
+
+    const json = await req.json();
+
+    if(!req.ok){
+        notFound();
+    }
+       
+    const coupon = CouponSchema.parse(json);
+    return coupon;
+}
+
+type Params = Promise<{id: string}>;
+
+export default async function EditCouponPage({params}: {params: Params}) {
+    const { id } = await params;
+    const coupon = await getCoupon(id);
+    
     return (
         <>
 
@@ -9,7 +30,7 @@ export default function EditCouponPage() {
                 Volver
             </Link>
 
-            <Heading>Editar Cupón: </Heading>
+            <Heading>Editar Cupón: {coupon.name}</Heading>
 
         </>
     )
