29c9c493944038e95230380d52310c3900782ad7
David <daviolivero2103@gmail.com>
2026-09-13 22:16:13 -0400

    feat(components): save edited coupon and redirect

--- src/components/coupons/EditCouponForm.tsx
+++ src/components/coupons/EditCouponForm.tsx
@@ -1,16 +1,29 @@
 "use client"
 
 import { updateCoupon } from "@/actions/update-coupon-action";
-import { useParams } from "next/navigation";
-import { ReactNode, useActionState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { ReactNode, useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function EditCouponForm({ children }: { children: ReactNode }) {
+    const router = useRouter();
     const { id } = useParams<{id: string}>();
     const updateCouponWithId = updateCoupon.bind(null, +id);
     const [state, dispatch] = useActionState(updateCouponWithId, {
         errors: [],
         success: ''
     });
+
+    useEffect(() => {
+        if(state.errors){
+            state.errors.forEach(error => toast.error(error))
+        }
+
+        if(state.success){
+            toast.success(state.success);
+            router.push('/admin/coupons')
+        }
+    }, [state]);
     
     return (
         <>
